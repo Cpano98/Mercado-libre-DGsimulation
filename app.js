221e72171dcff8df2e2f90517429ab901ea4d869
Carlos Pano H <carlospano8@gmail.com>
2021-12-02 15:47:00 -0600

    Listo hasta clase 15.
    Header. Productos. Hover

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
+//REGISTER
+app.get('/register', (req, res) =>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+//LOGIN
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 //Página de error
 app.get('/404', (req, res) => {
     res.send("Error. La página solicitada no esta disponible"); 
